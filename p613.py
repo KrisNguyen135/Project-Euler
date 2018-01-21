541838c7b83b93f0a13aba787f92cd99cf78a2a6
Quan Nguyen <nguyenminhquan135@gmail.com>
2018-01-21 16:20:14 -0500

    Add files via upload

---
+++ p613.py
@@ -0,0 +1,9 @@
+from math import pi, log
+
+if __name__ == '__main__':
+    result = 12 * pi + 32 * log(2) + 9 * log(3) - 25 * log(5)
+    result /= 24 * pi
+
+    print(result)
+
+    print('Done.')
